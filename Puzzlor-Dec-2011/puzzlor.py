ff72c847b273a7ed39304e6a9cf6830c77a62a6f
isms <ims11235@gmail.com>
2012-10-08 19:51:42 -0400

    cleaned up with pylint

--- Puzzlor-Dec-2011/puzzlor.py
+++ Puzzlor-Dec-2011/puzzlor.py
@@ -7,41 +7,55 @@ C = (2, 2, 2, 2, 6, 6)
 D = (3, 5, 3, 3, 5, 5)
 
 def roll(S):
+    """ return a result of rolling the given die """
     return random.sample(S, 1)[0]
 
 def playgame():
+    """ roll each die and return a dictionary of the results """
     return {roll(A):'A', roll(B):'B', roll(C):'C', roll(D):'D'}
 
 def get_winner(outcome):
+    """ return the winner as a string, e.g. 'A' """
     return outcome[max(outcome.keys())]
 
 def tally_outcomes(outcomes):
+    """ count up all the outcomes """
     tally = {'A':0, 'B':0, 'C':0,'D':0}
     for outcome in outcomes:
         tally[get_winner(outcome)] += 1
     return tally
 
 def probabilities(tally, N):
+    """ find the probabilities of each die """
     prob = {}
     for die, wins in tally.iteritems():
         prob[die] = float(wins) / N
     return prob
 
 def run_simulation(N):
+    """ play the game N times, return the outcomes dictionary """
     outcomes = []
     for i in range(N):
         outcomes.append(playgame())
     return outcomes
 
-# all possible outcomes
-# all_outcomes = [{a:'A', b:'B', c:'C', d:'D'} for a in A for b in B for c in C for d in D]
+def get_all_outcomes():
+    """ brute force all possible outcomes and return the probabilities """
+    return [{a:'A', b:'B', c:'C', d:'D'} for a in A for b in B for c in C for d in D]
 
-if __name__ == '__main__':
-    N = int(sys.argv[1])
-    print("Number of simulations: " + str(N))
-    tally = tally_outcomes(run_simulation(N))
+def print_results(outcomes):
+    """ summarize a set of outcomes """
+    N = len(outcomes)
+    print("Number of games: " + str(N))
+    tally = tally_outcomes(outcomes)
     print("Winners:")
     print(tally)
     print("Probabilities:")
     print(probabilities(tally, N))
 
+if (__name__ == '__main__'):
+    N = int(sys.argv[1])
+    print("SIMULATION")
+    print_results(run_simulation(N))
+    print("BRUTE FORCE")
+    print_results(get_all_outcomes())
--- airplane-problem/airplane.py
+++ airplane-problem/airplane.py
@@ -1,40 +1,40 @@
-'''
-One hundred people board a 100-seat airplane. The first one has lost his
+""" One hundred people board a 100-seat airplane. The first one has lost his
 boarding pass, so he sits in a random seat. Each subsequent passenger sits
 in his own seat if it's available or takes a random unoccupied seat if it's
 not.
 
-What's the probability that the 100th passenger finds his seat occupied?
-'''
+What's the probability that the 100th passenger finds his seat occupied? """
 
 import random
 import datetime
 import sys
 
-# create empty plane
 def create_empty_plane():
+    """ create empty plane """
     new_plane = {}
     for i in range(1,101):
         new_plane[i] = 0
     return new_plane
 
-# return all open seats
 def list_empty_seats(plane):
+    """ return all open seats """
     return [i for i in plane.keys() if plane[i] == 0]
 
-# if own seat open, take it; otherwise sit in random empty seat
 def pick_seat(plane, passenger):
+    """ if own seat open, take it; otherwise sit in random empty seat """
     if(plane[passenger] > 0):
-		pick_random_seat(plane, passenger)
+        pick_random_seat(plane, passenger)
     else:
-		plane[passenger] = passenger
+        plane[passenger] = passenger
 
 def pick_random_seat(plane, passenger):
+    """ seat the passenger in an empty seat """
     empty_seats = list_empty_seats(plane)
     random_seat = random.choice(empty_seats)
     plane[random_seat] = passenger
     
 def run_simulation():
+    """ go through and seat a passenger """
     plane = create_empty_plane() # create new plane
     pick_random_seat(plane, 1) # first passenger sits in random seat
     # for passengers 2 through 100, sit in own seat or random seat
@@ -43,6 +43,7 @@ def run_simulation():
     return plane
     
 def run_simulations(num_simulations):
+    """ go through a certain numer of simulations """
     # track time started
     time_start = datetime.datetime.now()    
     # initialize counter for successes
@@ -51,30 +52,20 @@ def run_simulations(num_simulations):
         plane = run_simulation()
         if(plane[100] == 100):
             success += 1
-        
     # track time ended
     time_stop = datetime.datetime.now()
-    
     proportion = float(success)/num_simulations
-    # print('Ran %d simulations, runtime: %s' % (num_simulations, time_stop-time_start))
-    # print('Passenger 100 sat in own seat: {0}/{1} {2:f}'.format(success, num_simulations, proportion))
     return proportion
     
-
-def usage():
-    print("Usage: python airplane.py [# samples] [# simulations each sample] ")
-
 if (__name__ == "__main__"):
     argv = sys.argv
     num_build = 0
-    
     try:
         num_samples = int(argv[1])
         num_simulations = int(argv[2])
     except:
-        usage()
-        sys.exit()
-    
+        print("Usage: python airplane.py [# samples] [# simulations each]")
+        sys.exit(1)
     outfile = open('output.txt', 'w')
     time_start = datetime.datetime.now()
     results = []
@@ -82,9 +73,8 @@ if (__name__ == "__main__"):
         result = run_simulations(num_simulations)
         print("Sample %d: %f" % (sample, result))
         outfile.write(str(result)+'\n')
-    
     time_stop = datetime.datetime.now()
     outfile.close()
-        
-    print('Ran %d samples of %d simulations each, runtime: %s' % (num_samples, num_simulations, time_stop-time_start))
+    print('Ran %d samples of %d simulations each, runtime: %s' % 
+        (num_samples, num_simulations, time_stop-time_start))
     
